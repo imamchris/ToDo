9fd80417594e7d917eb75355e6b68c412863feee
cpereira <cpereira@student.saintaug.nsw.edu.au>
2024-11-18 09:15:24 +1100

    Tried to fix Signup.db

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template, request, redirect, url_for, flash, session
 from sqlalchemy import create_engine, text
 import os, secrets
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask import Flask, render_template, request, redirect, url_for, flash, session
 
 app = Flask(__name__)
 app.secret_key = secrets.token_hex(16)
